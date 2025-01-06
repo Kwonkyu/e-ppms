4288271b7f1d522f36d7527e29a98b9266def8de
Kwonkyu <park2348190@naver.com>
2025-01-06 23:58:35 +0900

    추가된 공통 의존성에서 계정 권한(AccountRole) enum을 인바운드 포트의 계정 도메인에 추가

--- usecase/inbound/src/main/kotlin/partners/pms/core/port/inbound/account/model/AuthorizedAccount.kt
+++ usecase/inbound/src/main/kotlin/partners/pms/core/port/inbound/account/model/AuthorizedAccount.kt
@@ -1,12 +1,14 @@
 package partners.pms.core.port.inbound.account.model
 
+import enum.AccountRole
+
 data class AuthorizedAccount(
     val username: String,
     val password: String,
     val name: String,
     val type: String,
     val status: String,
-    val role: String,
+    val roles: Set<AccountRole>,
     val email: String,
     val isExpired: Boolean,
 )
